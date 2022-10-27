9ee6e83588485b7949dddf957ec8007e965e8a27
Khang1405 <108504628+Khang1405@users.noreply.github.com>
2022-10-27 21:50:20 +0700

    Toan tu

---
+++ cs03/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program 
+{
+    static void Main(string[] args)
+    {
+        int a = 6;
+        int b = 6;
+        Console.WriteLine("a + b = {0}",a + b);
+        Console.WriteLine("a - b = {0}",a - b);
+        Console.WriteLine("a * b = {0}",a * b);
+        Console.WriteLine("a / b = {0}",a / b);
+        Console.WriteLine("a % b = {0}",a % b);
+        // do uu tien ve toan hoc
+        int kq = a + b * 2;
+        Console.WriteLine(kq);
+    }
+}
