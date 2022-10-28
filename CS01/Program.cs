24e81c4577f6d91ab32ecdd1b46d0e1a464b18e0
Khang1405 <108504628+Khang1405@users.noreply.github.com>
2022-10-29 01:05:07 +0700

    Change everything new

---
+++ CS01/Program.cs
@@ -0,0 +1,18 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Minh la ong Khang");
+    }
+    /// <summary>
+    /// phuong thuc tra ve 2 so nguyen
+    /// </summary>
+    /// <param name="a">so nguyen 1</param>
+    /// <param name="b">so nguyen 2</param>
+    /// <returns>Tong a + b</returns>
+    static int tong(int a, int b)        
+    {
+        return (a + b);
+    }
+}
---
+++ CS04/Program.cs
@@ -0,0 +1,27 @@
+﻿class CS04
+{
+    static void Main(string[] args)
+    {
+        bool checkTF;
+        // checkTF = false;
+        checkTF = true;
+        Console.WriteLine(checkTF);
+        
+        int a = 6;
+        int b = 1;
+        
+        bool kq = a == b;
+        Console.WriteLine("a == b la {0}", kq);
+        Console.WriteLine("a != b la {0}", !kq);
+        Console.WriteLine($"a > b la {a > b}") ;
+        Console.WriteLine($"a >= b la {a >= b}") ;
+        Console.WriteLine($"a <  b la {a < b}");
+        Console.WriteLine($"a <= b la {a <= b}") ;
+
+        bool c = true;
+        bool d = false;
+        Console.WriteLine(c || d);
+        Console.WriteLine(c && d);
+
+    }
+}
