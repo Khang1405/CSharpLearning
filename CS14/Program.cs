ef189c703e0318a128682c72eeef31b1c24ccc5a
Khang1405 <108504628+Khang1405@users.noreply.github.com>
2022-11-02 14:23:55 +0700

    Tinh ke thua

---
+++ CS14/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace CS14
+{
+    class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Khoi tao Animal");
+        }
+        public Animal(string abc)
+        {
+            Console.WriteLine($"Khoi tao {abc}");
+        }
+        public int Legs {set; get;}
+        public float Weight{set; get;}
+
+        public void ShowLegs()
+        {
+            Console.WriteLine($"Legs = {Legs}");
+        }
+    }
+    class Cat : Animal
+    {
+        public Cat(string s) : base(s)
+        {
+            this.Legs = 4;
+            this.Food = "Mouse";
+            Console.WriteLine("Khoi tao lop Cat");
+        }
+        public string Food;
+        public new void ShowLegs()
+        {
+            Console.WriteLine($"Con meo co {Legs} chan");
+        }
+        public void Eat ()
+        {
+            Console.WriteLine(Food);
+        }
+        public void GetInfo()
+        {
+            base.ShowLegs();
+            ShowLegs();
+        }
+    }
+    class A {}
+    class B: A{};
+    class C: B{};
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Cat c = new Cat("Anh Khang");
+            // c.ShowLegs();
+            // c.Eat();
+            // c.GetInfo();
+        }
+    }
+}
