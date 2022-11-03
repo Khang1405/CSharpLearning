e177de0726ca2d9cbb34dfad312daecb75cad828
Khang1405 <108504628+Khang1405@users.noreply.github.com>
2022-11-03 16:00:57 +0700

    null, nullable

---
+++ CS18/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CS18
+{
+    class bac 
+    {
+        public void xinchao()
+        {
+            Console.WriteLine("Hehehe, Siu");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bac a = new bac();
+            a?.xinchao();
+            // tham tri khong the nhan gia tri null nhung co the nullable
+            int? name;
+            name = null;
+            name = 10;
+            if(name.HasValue)
+            {
+                int _name = name.Value;
+                Console.WriteLine(_name);
+            }
+        }
+    }
+}
