c2eccc7021c89d0270452737e3366ca2ed8e0cd7
Khang1405 <108504628+Khang1405@users.noreply.github.com>
2022-11-03 15:47:33 +0700

    Anonymous and Dynamic

---
+++ CS17/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace CS17
+{
+    class SinhVien 
+    {
+        public string hoTen {get; set;} = default!;
+        public int namSinh{get; set;}
+        public string noiSinh {get; set;} = default!;
+        public void InfSinhVien()
+        {
+            Console.WriteLine(hoTen);
+        }
+    }
+    class Program 
+    {
+        
+        static void printIF(dynamic obj)
+        {
+            obj.hoTen = "Khangg";
+            obj.InfSinhVien();
+        }  
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Siuu");
+            // var sanpham1 = new 
+            // {
+            //     Gia = 10000,
+            //     TenSP = "Dien thoai",
+            //     NamSX = 2020
+            // };
+            // Console.WriteLine(sanpham1.Gia);
+            // Console.WriteLine(sanpham1.NamSX);
+            List<SinhVien> listSV = new List<SinhVien>()
+            {
+                new SinhVien() {hoTen = "khang", namSinh= 2002 , noiSinh = "Gia Lai"},
+                new SinhVien() {hoTen = "khan", namSinh = 2001 ,noiSinh = "Quang Nam"},
+                new SinhVien() {hoTen = "kha", namSinh = 2000,noiSinh = "Quang Ngai"}
+
+            };
+            var ketqua = from sv in listSV
+                        where sv.hoTen.Contains("n")
+                        select new 
+                        {
+                            Ten = sv.hoTen,
+                            Nam = sv.namSinh
+                        };
+            foreach (var sv in ketqua)
+            {
+                Console.WriteLine(sv.Nam  + " " + sv.Ten);
+            }
+
+            SinhVien sv1 = new SinhVien();
+            printIF(sv1);
+        }
+    }
+}
