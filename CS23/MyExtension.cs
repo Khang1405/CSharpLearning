524e0e149cc044b2ab937c7e37f3bd4caf6a4d90
Khang1405 <108504628+Khang1405@users.noreply.github.com>
2022-11-08 21:39:06 +0700

    Extension method

---
+++ CS23/MyExtension.cs
@@ -0,0 +1,11 @@
+namespace myLib
+{
+    public static class A
+    {
+        public static double BinhPhuong (this double a)  => a * a;
+        public static double CanBacHai (this double a)  => Math.Sqrt(a);
+        public static double Sin (this double a)  => Math.Sin(a);
+        public static double Cos (this double a)  => Math.Cos(a);
+    }
+
+}
---
+++ CS23/Program.cs
@@ -0,0 +1,31 @@
+﻿using myLib;
+namespace CS23
+{
+    // Xay dung lop tinh
+    // tro thanh 1 phuong thuc mo rong bang cach them this
+    static class ABC
+    {
+        public static void Print(this string s, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+        }
+    }
+    class Program
+    {
+           static void Main(string[] args)
+        {
+            int [] mang = {3,4,6,1,7};
+            int max = mang.Max();
+            Console.WriteLine(max);
+            string s = "KhangHC123";
+            s.Print(ConsoleColor.Red);
+            double a = 5;
+            Console.WriteLine(a.BinhPhuong());
+            Console.WriteLine(a.CanBacHai());
+            Console.WriteLine(a.Sin());
+            Console.WriteLine(a.Cos());
+
+        }
+    }
+}
