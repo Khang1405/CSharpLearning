ccb1b95717961e1a619aa0539f4929854bee9b0b
Khang1405 <108504628+Khang1405@users.noreply.github.com>
2022-11-11 14:39:35 +0700

    Queue, Stack, HashSet

---
+++ CS27/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CS27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Queue
+            Queue<string> cacHoSo = new Queue<string>();
+            cacHoSo.Enqueue("Ho so 1");
+            cacHoSo.Enqueue("Ho so 2");
+            cacHoSo.Enqueue("Ho so 3");
+            foreach(var hs in cacHoSo)
+            {
+                Console.WriteLine(hs);  
+            }
+            var hoso = cacHoSo.Dequeue();
+            Console.WriteLine($"Xu ly ho so: {hoso}-{cacHoSo.Count}");
+            // Stack
+            Stack<string> hanghoa = new Stack<string>();
+            hanghoa.Push("Ho so 1");
+            hanghoa.Push("Ho so 2");
+            hanghoa.Push("Ho so 3");
+            var mathang = hanghoa.Pop();
+            Console.WriteLine($"Lay ra {mathang}");
+            // LinkedList
+            LinkedList<string> cacbaihoc new LinkedList<string>();
+            var bh1 = cacbaihoc.AddFirst("Bai hoc 1");
+            var bh3 = cacbaihoc.AddLast("Bai hoc 3");
+            LinkedListNode<string> bh2 = cacbaihoc.AddAfter(bh1,"Bai hoc 2");
+        }
+    }
+}
